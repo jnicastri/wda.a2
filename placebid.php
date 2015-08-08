dfeab2a39476e3b9509021ce9e63034202b1fcea
JimmiN <jimmi.nicastri@outlook.com>
2015-08-08 16:46:03 +1000

    Bid validation tweaks

--- placebid.php
+++ placebid.php
@@ -1,32 +1,44 @@
 <?php
 	require_once("includes/requirebundle.php");
 	
-	$listId = tryParseToNull($_GET["id"], "int");
-	
-	$temp = new MiniTemplator;
-	$load = $temp->readTemplateFromFile("html/tmpl-placebid.html");
+	if(!is_null($_SESSION[SESSION_USER_KEY])){
 		
-	if(!$load)
-		die ("Loading template has failed!");
-	
-	if(is_null($listId))
-		header("Location: index.php");
+		$listId = tryParseToNull($_GET["id"], "int");
 		
-	loadPage($listId, $temp);
-	$temp->generateOutput();
+		$temp = new MiniTemplator;
+		$load = $temp->readTemplateFromFile("html/tmpl-placebid.html");
+			
+		if(!$load)
+			die ("Loading template has failed!");
+		
+		if(is_null($listId)){
+			header("Location: index.php");
+			exit();
+		}
+			
+		loadPage($listId, $temp);
+		$temp->generateOutput();		
+	}
+	else{
+		header("Location: login.php");
+		exit();
+	}
+	
 	
 	function loadPage($listId, &$temp){
 			
 		$listing = Listing::LoadById($listId);
 		
-		if(is_null($listing))
-			header("Location: index.php"); 
-			
+		if(is_null($listing)){
+			header("Location: index.php");
+			exit(); 
+		}
 		$item = Item::LoadById($listing->ItemId);
 		
-		if(is_null($item))
-			header("Location: index.php"); 
-			
+		if(is_null($item)){
+			header("Location: index.php");
+			exit(); 
+		}
 		$temp->setVariable("i_title", $item->Name);
 		$temp->setVariable("l_bidamt", "$".strval(number_format($listing->BidIncrementAmount,2)));
 		$temp->setVariable("l_end", strval($listing->EndDate));
@@ -38,5 +50,6 @@
 		? $temp->setVariable("l_top", "$".strval(number_format($topBid->BidValue,2))) 
 		: $temp->setVariable("l_top", "No Bids");
 	}
+	
 	
 ?>
